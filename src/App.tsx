0b8f6263f15d210944ec6d8766615dcd02ae77f0
Kafilat <kafilatadewumi61@gmail.com>
2023-03-04 18:31:36 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+
+
+function App() {
+
+  const router = createBrowserRouter([
+   {
+    path: "/",
+    element: "",
+   },
+  
+  ]);
+
+  return <RouterProvider router = {router} />
+}
+
+export default App
